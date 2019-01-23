f5721d4b051a0356ea8673740019f3582aad3eb2
Addy1293 <mrcool82@gmail.com>
2019-01-23 16:03:15 +0200

    jq

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-var cupsofCoffee
-var numberofGirls
-var projectsCompleted
-var hoursofCoding
+// jQuery(document).ready(function() {
+//     var offset = 220;
+//     var duration = 500;
+//     jQuery(window).scroll(function() {
+//         if (jQuery(this).scrollTop() > offset) {
+//             jQuery('.crunchify-top').fadeIn(duration);
+//         } else {
+//             jQuery('.crunchify-top').fadeOut(duration);
+//         }
+//     });
+
+//     jQuery('.crunchify-top').click(function(event) {
+//         event.preventDefault();
+//         jQuery('html, body').animate({scrollTop: 0}, duration);
+//         return false;
+//     })
+// });
+
+
+
+
+
+
+var timeLeftForCoffee
+var timeLeftForGirls
+var timeLeftForProjectsCompleted
+var timeLeftForCoding
+
+document.getElementById('counter').innerHTML
